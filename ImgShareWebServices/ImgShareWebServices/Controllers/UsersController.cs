aa5f11407156f383456c64c3f850decaf685e6dd
RafaelaRuchinski <rafaelaruchi@gmail.com>
2018-06-26 07:46:00 -0300

    Web Service

---
+++ ImgShareWebServices/ImgShareWebServices/Controllers/UsersController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ImgShareWebServices.Models;
+
+namespace ImgShareWebServices.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        private readonly ImgShareContext _context;
+
+        public UsersController(ImgShareContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<User> GetUser()
+        {
+            return _context.User;
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var User = await _context.User.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(User);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser([FromRoute] int id, [FromBody] User User)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != User.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(User).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User User)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.User.Add(User);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = User.Id }, User);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var User = await _context.User.SingleOrDefaultAsync(m => m.Id == id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+
+            _context.User.Remove(User);
+            await _context.SaveChangesAsync();
+
+            return Ok(User);
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.User.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ImgShareWebServices/ImgShareWebServices/Data/ImgShareContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImgShareWebServices.Models
+{
+    public class ImgShareContext : DbContext
+    {
+        public ImgShareContext (DbContextOptions<ImgShareContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ImgShareWebServices.Models.Photo> Photo { get; set; }
+        public DbSet<ImgShareWebServices.Models.User> User { get; set; }
+    }
+}
